af05a3c2a4d6effdf5f56406d65c7fbc5f72bdc4
jog225 <jog225@lehigh.edu>
2022-06-15 13:02:46 -0400

    App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from './logo.svg';
+//import './App.css';
+import FC from "./Components/FunctionalComp";
+import {ClassComp1} from "./Components/ClassComp";
+import Click from "./Components/Click";
+import Counter from "./Components/Counter";
+import ParentComp from "./Components/ParentComp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Tutorial to Learn React</h1>
+      <h2>More about Components</h2>
+      <FC />
+      {/*<ClassComp /> */}
+      <ClassComp1 />
+      <Click />
+      <Counter />
+      <ParentComp />
     </div>
   );
 }
